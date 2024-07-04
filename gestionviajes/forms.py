0efb9be237e74414e44ab8e4053d3ae926a5e75a
Beayemi <emiliopardocantillana@gmail.com>
2024-07-03 22:55:42 -0400

    Edit 2 Funciona La Base De Datos

--- gestionviajes/forms.py
+++ gestionviajes/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Contract, Customer
 
+
 class ContractForm(forms.ModelForm):
     rut = forms.CharField(max_length=12)
     name = forms.CharField(max_length=100)
-    
+    departure_date = forms.DateField(widget=forms.SelectDateWidget())
+    return_date = forms.DateField(widget=forms.SelectDateWidget())
+
     class Meta:
         model = Contract
         fields = ['rut', 'name', 'destination', 'departure_date', 'return_date']
-
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
@@ -19,3 +21,4 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
+1
